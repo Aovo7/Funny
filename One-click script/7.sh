ec2a829683ef9ac0e7b2b7a739dac4b196142da8
Aovo7 <134157338+Aovo7@users.noreply.github.com>
2023-11-25 16:43:16 +0800

    Update 7.sh

--- One-click script/7.sh
+++ One-click script/7.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
-echo "1. 一键Snell"
-echo "2. 一键SS"
+echo "1.一键Snell"
+echo "2.一键SS"
+echo "3.一键TUIC"
+echo "4.一键Hy2"
+echo "5.一键Singbox"
+echo "6.一键V2Ray(VMESS/Trojan+WS/gRPC/TCP(+TLS)"
 echo "请输入数字:"
 read number
 
-if [ "$number" = "1" ]; then
-    wget -O snell.sh --no-check-certificate https://git.io/Snell.sh && chmod +x snell.sh && ./snell.sh
-elif [ "$number" = "2" ]; then
-    wget -O ss-rust.sh --no-check-certificate https://raw.githubusercontent.com/xOS/Shadowsocks-Rust/master/ss-rust.sh && chmod +x ss-rust.sh && ./ss-rust.sh
-else
-    echo "请输入正确的数字哦"
-fi
+case $number in
+    0)
+        exit 0
+        ;;
+    1)
+        wget -O snell.sh --no-check-certificate https://git.io/Snell.sh && chmod +x snell.sh && ./snell.sh
+        ;;
+    2)
+        wget -O ss-rust.sh --no-check-certificate https://raw.githubusercontent.com/xOS/Shadowsocks-Rust/master/ss-rust.sh && chmod +x ss-rust.sh && ./ss-rust.sh
+        ;;
+    3)
+        wget -N --no-check-certificate https://raw.githubusercontent.com/CCCOrz/auto-tuic/main/tuic.sh && bash tuic.sh
+        ;;
+    4)
+        wget -N --no-check-certificate https://raw.githubusercontent.com/Misaka-blog/hysteria-install/main/hy2/hysteria.sh && bash hysteria.sh
+        ;;
+    5)
+        wget -N -O /root/singbox.sh https://raw.githubusercontent.com/TinrLin/sing-box/main/Install.sh && chmod +x /root/singbox.sh && ln -sf /root/singbox.sh /usr/local/bin/singbox && bash /root/singbox.sh
+        ;;
+    6)
+        bash <(wget -qO- -o- https://git.io/v2ray.sh)
+        ;;
+    *)
+        echo "请输入正确的数字哦"
+        ;;
+esac
